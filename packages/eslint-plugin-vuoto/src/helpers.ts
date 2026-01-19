ff1949ec3f425cd84ec47a29a6c851de5c48ff51
Davide Di Criscito <dcdavidev@gmail.com>
2026-01-19 10:19:05 +0100

    fix(eslint-plugin-vuoto): package.json path

--- packages/eslint-plugin-vuoto/src/helpers.ts
+++ packages/eslint-plugin-vuoto/src/helpers.ts
@@ -3,6 +3,6 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const packageJsonPath = path.resolve(__dirname, '../../package.json');
+const packageJsonPath = path.resolve(__dirname, '../package.json');
 
 export const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
